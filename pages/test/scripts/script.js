f7263d955d9f3a96367e3f10ede6366eccc749a9
SArthurX <sxa05z@gmail.com>
2023-02-02 16:34:12 +0800

    pass

---
+++ pages/test/scripts/script.js
@@ -0,0 +1,8 @@
+const { createApp, ref } = Vue;
+  const vm = createApp({
+      setup (){
+        const message = ref('Hello Vue 3.0!');
+        return {message}
+      }
+  });
+vm.mount('#app');
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
 function goPage(url, time) {
     setTimeout(() => window.location.href=url, time);
 }
+
+var password = 0000;
+var input;
+
+while(password!=input){
+    input=prompt("請輸入密碼");
+}
+
+alert("登入成功")
+
+if(password=input){
+    location.href = 'https://sarthurx.tk/header.html';
+}
